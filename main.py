fa79f4b91bc5e0753b9a1e7c337a0f089d54a547
agustin-costa-dpv <agucosta@gmail.com>
2026-04-28 14:17:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ async def webhook(request: Request):
         # Consulta real -> Groq
         try:
             chat_completion = client.chat.completions.create(
-                model="llama3-8b-8192",
+                model="llama-3.1-8b-instant",
                 messages=[
                     {"role": "system", "content": SYSTEM_PROMPT},
                     {"role": "user", "content": text}
